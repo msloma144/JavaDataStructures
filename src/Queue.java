9e76bfcf067e286c251d4e91f05817331f29a899
msloma144 <syphr144@gmail.com>
2017-11-13 21:04:03 -0500

    Bug Fixes

--- src/Queue.java
+++ src/Queue.java
@@ -5,36 +5,73 @@ import java.util.ArrayList;
  */
 public class Queue<T> {
     private ArrayList<T> queue;
+    private int MAXSIZE;
+    private int front, rear, size;
 
     public Queue(){
         this.queue = new ArrayList<>();
+        this.MAXSIZE = -1;
+        this.front = 0;
+        this.rear = MAXSIZE - 1;
+        this.size = 0;
+    }
+
+    public Queue(int MAXSIZE){
+        this.queue = new ArrayList<>();
+        this.MAXSIZE = MAXSIZE;
+        for(int i = 0; i < this.MAXSIZE; i++){
+            queue.add(null);
+        }
+        this.front = 0;
+        this.rear = MAXSIZE - 1;
+        this.size = 0;
     }
 
     public void enqueue(T element){
-        queue.add(element);
+        if (isFull()) {
+            System.out.println("Queue is full!");
+        }
+        else if(MAXSIZE == -1){
+            queue.add(0, element);
+            size++;
+        }
+        else {
+            rear = (rear + 1) % MAXSIZE;
+            queue.set(rear, element);
+            size++;
+        }
     }
 
     public T dequeue(){
-        int endQueue = queue.size() - 1;
+        if (isEmpty()) {
+            System.out.println("Dequeue attempted on empty queue!");
+            return null;
+        }
+        else if(MAXSIZE == -1){
+            int endQueue = queue.size() - 1;
 
-        if(!isEmpty()){
             T temp = queue.get(endQueue);
             queue.remove(endQueue);
+            size--;
             return temp;
         }
         else {
-            System.out.println("Dequeue attempted on empty queue!");
-            return null;
+            T tmp = queue.get(front);
+            this.front = (this.front + 1) % MAXSIZE;
+            this.size--;
+            return tmp;
         }
     }
 
     boolean isEmpty(){
-        if(queue.size() == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return this.size == 0;
     }
 
+    boolean isFull(){
+        return this.size == MAXSIZE;
+    }
+
+    public int size() {
+        return size;
+    }
 }
